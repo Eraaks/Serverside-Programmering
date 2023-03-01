edf0cb24f2579ea3ef848d065b3152b035896c99
Oliver L. Jensen <Oliver.jensen@3shape.com>
2023-03-01 11:38:30 +0100

    Add project files.

---
+++ ServerSideBlazorApp1/Codes/EncryptionTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace ServerSideBlazorApp1.Codes
+{
+    public class EncryptionTest
+    {
+        private readonly IDataProtector _dataProtector;
+        public EncryptionTest(IDataProtectionProvider dataProtector)
+        {
+            _dataProtector = dataProtector.CreateProtector("BlazoreServerApp1.codes.EncryptionTest.Oliver");
+        }
+
+        public string Protect(string valueToEncrypt) => _dataProtector.Protect(valueToEncrypt);
+        public string UnProtect(string valueToDecrypt) => _dataProtector.Unprotect(valueToDecrypt);
+    }
+}
